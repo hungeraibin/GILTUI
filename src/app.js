188690f6c8ea5664c1266e68b8ba913cb14fc1bf
王爱彬 <hungeraibin@126.com>
2018-12-18 20:21:01 +0800

    创建 button group

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
   data: {
-    loading1: false
+    loading1: false,
+    loading2: true,
+    loading3: false
   }
 })
