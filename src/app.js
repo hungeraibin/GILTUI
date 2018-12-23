aba8b058ffda169255f253153f246ba025da8a8e
hungeraibin <hungeraibin@126.com>
2018-12-23 23:37:59 +0800

    基本实现 row 和 col 的功能

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
-
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 new Vue({
   el: '#app',
@@ -25,101 +28,3 @@ new Vue({
   }
 })
 
-// 单元测试
-import chai from 'chai'
-import spies from 'chai-spies'
-
-chai.use(spies)
-const expect = chai.expect
-
-try {
-  // 测试按钮含有 settings icon
-  {
-    const Constructor = Vue.extend(Button) // Button是对象 转化为 构造器（函数）
-    const vm = new Constructor({
-      propsData: {
-        icon: 'settings'
-      }
-    }) // VueComponent实例
-    vm.$mount() // 动态生成按钮
-    let useElement = vm.$el.querySelector('use')
-    let herf = useElement.getAttribute('xlink:href')
-    expect(herf).to.eq('#i-settings')
-    vm.$el.remove()
-    vm.$destroy()
-  }
-  // 测试按钮含有 loading icon
-  {
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'loading',
-        loading: true
-      }
-    }) 
-    vm.$mount() 
-    let useElement = vm.$el.querySelector('use')
-    let herf = useElement.getAttribute('xlink:href')
-    expect(herf).to.eq('#i-loading')
-    vm.$el.remove()
-    vm.$destroy()
-  }
-  // 测试按钮 svg order
-  {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'settings'
-      }
-    }) 
-    vm.$mount(div) 
-    let svg = vm.$el.querySelector('svg')
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq('1')
-    vm.$el.remove()
-    vm.$destroy()
-  }
-  // 测试按钮 svg order
-  {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'settings',
-        iconPosition: 'right'
-      }
-    }) 
-    vm.$mount(div) 
-    let svg = vm.$el.querySelector('svg')
-    let { order } = window.getComputedStyle(svg)
-    expect(order).to.eq('2')
-    vm.$el.remove()
-    vm.$destroy()
-  }
-  // 测试按钮点击事件
-  {
-    // mock
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-      propsData: {
-        icon: 'settings'
-      }
-    }) 
-    vm.$mount() 
-    let spy = chai.spy(function() {})
-    vm.$on('click', spy)
-    vm.$el.click()
-    expect(spy).to.have.been.called()
-    vm.$el.remove()
-    vm.$destroy()
-  }  
-} catch (error) {
-  window.errors = [error]
-} finally {
-  window.errors && window.errors.forEach(error => {
-    console.error(error.message)
-  })
-}
---
+++ src/row.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row" :style="{marginLeft: `${-gutter/2}px`, marginRight: `${-gutter/2}px`}">
+    <slot></slot>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuluRow',
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  // created 数据观测 (data observer)，属性和方法的运算，watch/event 事件回调
+  // mounted el 被新创建的 vm.$el 替换，并挂载到实例上去之后调用该钩子。
+  mounted() {
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .row {
+    display: flex;
+  }
+</style>
+
+
