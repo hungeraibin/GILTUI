67c0bdd79601947777995ed920476c7b847555e5
hungeraibin <hungeraibin@126.com>
2018-12-25 12:14:20 +0800

    响应式基本完成

--- src/col.vue
+++ src/col.vue
@@ -72,7 +72,7 @@ export default {
         margin-left: ($n / 24) * 100%;
       }
     }
-    @media (min-width: 577px) and (max-width: 768) {
+    @media (min-width: 577px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -86,7 +86,7 @@ export default {
         }
       }
     }
-     @media (min-width: 769px) and (max-width: 992px) {
+     @media (min-width: 769px) {
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -100,7 +100,7 @@ export default {
         }
       }
     }
-    @media (min-width: 993px) and (max-width: 1200px) {
+    @media (min-width: 993px) {
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
