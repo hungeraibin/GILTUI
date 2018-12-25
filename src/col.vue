f5414bda95f97b10f91a8e5f26cbfe05ecd869bb
hungeraibin <hungeraibin@126.com>
2018-12-25 15:22:08 +0800

    完成 row  和 col 的单元测试

--- src/col.vue
+++ src/col.vue
@@ -38,13 +38,13 @@ export default {
   computed: {
     colClass() {
       let { span, offset, ipad, narrowPc, pc, widePc } = this
+      let createClasses = this.createClasses
       return [
-        span && `col-${span}`,
-        offset && `offset-${offset}`,
-        ipad && `col-ipad-${ipad.span}`,
-        narrowPc && `col-narrow-pc-${narrowPc.span}`,
-        pc && `col-pc-${pc.span}`,
-        widePc && `col-wide-pc-${widePc.span}`,
+        ...createClasses({span, offset}),
+        ...createClasses(ipad, 'ipad-'),
+        ...createClasses(narrowPc, 'narrowPc-'),
+        ...createClasses(pc, 'pc-'),
+        ...createClasses(widePc, 'widePc-'),
       ]
     },
     colStyle() {
@@ -53,6 +53,15 @@ export default {
         paddingLeft: `${gutter/2}px`, 
         paddingRight: `${gutter/2}px`
       }
+    }
+  },
+  methods: {
+    createClasses(obj, str='') {
+      if (!obj) { return [] }
+      let array = []
+      if (obj.span) { array.push(`col-${str}${obj.span}`) }
+      if (obj.offset) { array.push(`offset-${str}${obj.offset}`) }
+      return array
     }
   }
 }
