a817e2bc8142b3edf7e63ecc54916971f4a6ceef
王爱彬 <hungeraibin@126.com>
2018-12-27 20:55:28 +0800

    完成 toast 测试用例

--- test/toast.test.js
+++ test/toast.test.js
@@ -6,9 +6,11 @@ Vue.config.productionTip = false
 Vue.config.devtools = false
 
 describe('Toast', () => {
-  it('存在', () => {
+
+  it('存在.', () => {
     expect(Toast).to.exist
   })
+
   describe('props', function () {
     it('接受 autoClose', (done) => {
       let div = document.createElement('div')
@@ -16,8 +18,7 @@ describe('Toast', () => {
       const Constructor = Vue.extend(Toast)
       const vm = new Constructor({
         propsData: {
-          autoClose: true,
-          autoCloseDelay: 1
+          autoClose: 1,
         }
       }).$mount(div)
       vm.$on('close', () => {
@@ -25,5 +26,40 @@ describe('Toast', () => {
         done()
       })
     })
+    it('接受 closeButton', () => {
+      const callback = sinon.fake();
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {
+          closeButton: {
+            text: '关闭吧',
+            callback,
+          },
+        }
+      }).$mount()
+      let closeButton = vm.$el.querySelector('.close')
+      expect(closeButton.textContent.trim()).to.eq('关闭吧')
+      closeButton.click()
+      expect(callback).to.have.been.called
+    })
+    it('接受 enableHtml', () => {
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {enableHtml: true}
+      })
+      vm.$slots.default = ['<strong id="test">hi</strong>']
+      vm.$mount()
+      let strong = vm.$el.querySelector('#test')
+      expect(strong).to.exist
+    })
+    it('接受 position', () => {
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {
+          position: 'bottom'
+        }
+      }).$mount()
+      expect(vm.$el.classList.contains('position-bottom')).to.eq(true)
+    })
   })
-})
+}) 
