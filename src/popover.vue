251bb196b7042a2e6a774fc5ab6ef2c9c970e465
王爱彬 <hungeraibin@126.com>
2019-01-03 18:58:07 +0800

    支持部分定位

--- src/popover.vue
+++ src/popover.vue
@@ -1,39 +1,151 @@
 <template>
-  <div class="popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div ref="popover" class="popover" @click="onClick">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" :class="{[`position-${position}`]: true}">
       <slot name="content"></slot>
     </div>
-    <slot ></slot>
+    <span ref="triggerWrapper" style="display: inline-block;">
+      <slot ></slot>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'GuluPopover',
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+      validator(value) {
+        return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+      }
+    }
+  },
   data() {
     return {
       visible: false
     }
   },
   methods: {
-    xxx() {
-      this.visible = !this.visible
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible === true) {
+          this.close()
+        } else {
+          this.open()
+        }
+      }
+    },
+    open() {
+      this.visible = true
+      this.$nextTick(() => {
+        this.positonContent()
+        document.addEventListener('click', this.onClickDocument)
+      })
+    },
+    positonContent() {
+      document.body.appendChild(this.$refs.contentWrapper)
+      const { contentWrapper, triggerWrapper } = this.$refs
+      let { width, height, top, left } = triggerWrapper.getBoundingClientRect()
+      if (this.positon === 'top') {
+        contentWrapper.style.left = left + window.scrollX + 'px'
+        contentWrapper.style.top = top + window.scrollY + 'px'
+      } else if (this.position === 'bottom') {
+        contentWrapper.style.left = left + window.scrollX + 'px'
+        contentWrapper.style.top = top + height + window.scrollY + 'px'
+      } else if (this.position === 'left') {
+        contentWrapper.style.left = left + window.scrollX + 'px'
+        let { height: height2 } = triggerWrapper.getBoundingClientRect()
+        contentWrapper.style.top = top + window.scrollY + (height - height2) / 2 + 'px'
+      }
+
+    },
+    onClickDocument(e) {
+      if (this.$refs.popover.contains(e.target) || this.$refs.contentWrapper.contains(e.target)) {
+        return
+      }
+      this.close()
+    },
+    close() {
+      this.visible = false
+      document.removeEventListener('click', this.onClickDocument)
     }
-  }
+  },
+
 }
 </script>
 
 <style lang="scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  .content-wrapper {
+}
+.content-wrapper {
+  position: absolute;
+  border: 1px solid red;
+  // box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, .5));
+  background-color: white;
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  padding: .5em 1em;
+  max-width: 20em;
+  word-break: break-all;
+  &::before, &::after {
+    content: '';
+    display: block;
+    border: 10px solid transparent;
+    width: 0;
+    height: 0;
     position: absolute;
-    bottom: 100%;
-    left: 0;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+  }
+  &.position-top {
+    transform: translateY(-100%);
+    margin-top: -10px;
+    &::before, &::after {
+      left: 10px;
+    }
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
+  }
+  &.position-bottom {
+    margin-top: 10px;
+    &::before, &::after {
+      left: 10px;
+    }
+    &::before {
+      border-bottom-color: black;
+      bottom: 100%;
+    }
+    &::after {
+      border-bottom-color: white;
+      bottom: calc(100% - 1px);
+    }
+  }
+  &.position-left {
+    transform: translateX(-100%);
+    margin-left: -10px;
+    &::before, &::after {
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    &::before {
+      border-left: black;
+      left: 100%;
+    }
+    &::after {
+      border-left-color: white;
+      left: calc(100% - 1px);
+    }
   }
 }
 </style>
--- src/tabs.vue
+++ src/tabs.vue
@@ -34,19 +34,27 @@ export default {
     }
   },
   mounted() {
-    if (this.$children.length === 0) {
-      console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
-    }
-    this.$children.forEach((vm) => {
-      if (vm.$options.name === 'GuluTabsHead') {
-        vm.$children.forEach((childVm) => {
-          if (childVm.$options.name === 'GuluTabsItem' 
-            && childVm.name === this.selected) {
-            this.eventBus.$emit('update:selected', this.selected, childVm)
-          }
-        })
+    this.checkChildren()
+    this.selectTab()
+  },
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
       }
-    })
+    },
+    selectTab() {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === 'GuluTabsHead') {
+          vm.$children.forEach((childVm) => {
+            if (childVm.$options.name === 'GuluTabsItem' 
+              && childVm.name === this.selected) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
+    }
   }
 }
 </script>
