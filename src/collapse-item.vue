2b2ef90dec07b39aaa70e79d6cd9ab966afba7d3
王爱彬 <hungeraibin@126.com>
2019-01-04 16:39:22 +0800

    实现不太好的single方式

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open = !open">
+    <div class="title" @click="toggle">
       {{ title }}
     </div>
     <div class="content" v-if="open">
@@ -11,7 +11,8 @@
 
 <script>
 export default {
-  name: ' GuluCollapseItem',
+  name: 'GuluCollapseItem',
+  inject: ['eventBus'],
   props: {
     title: {
       type: String,
@@ -21,6 +22,26 @@ export default {
   data() {
     return {
       open: false
+    }
+  },
+  mounted() {
+    this.eventBus && this.eventBus.$on('update:selected', (vm) => {
+      if (vm !== this) {
+        this.close()
+      }
+    })
+  },
+  methods: {
+    toggle() {
+      if (this.open) {
+        this.open = false
+      } else {
+        this.open = true
+        this.eventBus && this.eventBus.$emit('update:selected', this)
+      }
+    },
+    close() {
+      this.open = false
     }
   }
 }
