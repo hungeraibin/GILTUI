6520e04c17bfb251a5fce50e5ffbca222b85acbf
王爱彬 <hungeraibin@126.com>
2019-01-11 16:44:15 +0800

    添加 v-click-outside 指令

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="cascader" ref="cascader">
+  <div class="cascader" v-click-outside="close">
     <div class="trigger" @click="toggle">
       {{ result || '&nbsp;' }}
     </div>
@@ -11,10 +11,12 @@
 
 <script>
 import CascaderItems from "./cascader-items";
+import ClickOutside from "./click-outside";
 
 export default {
   name: "GuluCascader",
   components: { CascaderItems },
+  directives: { ClickOutside },
   props: {
     source: {
       type: Array
@@ -40,23 +42,11 @@ export default {
     }
   },
   methods: {
-    onClickDocument(e) {
-      let { cascader } = this.$refs;
-      let { target } = e;
-      if (cascader === target || cascader.contains(target)) {
-        return;
-      }
-      this.close();
-    },
     open() {
       this.popoverVisible = true;
-      this.$nextTick(() => {
-        document.addEventListener("click", this.onClickDocument);
-      });
     },
     close() {
       this.popoverVisible = false;
-      document.removeEventListener("click", this.onClickDocument);
     },
     toggle() {
       if (this.popoverVisible === true) {
--- src/demo.vue
+++ src/demo.vue
@@ -1,12 +1,14 @@
 <template>
   <div style="padding: 20px">
     <g-cascader :source="source" :selected.sync="selected" :load-data="loadData" @update:source="onUpdateSource"></g-cascader>
+    <g-cascader :source="source" :selected.sync="selected" :load-data="loadData" @update:source="onUpdateSource"></g-cascader>
   </div>
 </template>
 
 <script>
 import Cascader from "./cascader";
 import db from "./db";
+import { removeListener } from "./click-outside";
 
 function ajax(parentId = 0) {
   return new Promise(success => {
@@ -34,6 +36,9 @@ export default {
       selected: [],
       source: []
     };
+  },
+  destroyed() {
+    removeListener();
   },
   created() {
     ajax(0).then(result => {
