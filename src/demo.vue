04a992083eeace6ef2162432b9378d20bd8c56cf
王爱彬 <hungeraibin@126.com>
2019-01-18 16:37:39 +0800

    使用依赖注入实现跨级调用

--- src/demo.vue
+++ src/demo.vue
@@ -2,7 +2,12 @@
   <div style="padding: 20px">
     <g-nav :selected.sync="selected">
       <g-nav-item name="home">首页</g-nav-item>
-      <g-nav-item name="about">关于</g-nav-item>
+      <g-sub-nav>
+        <template slot="title">关于</template>  
+        <g-nav-item name="culture">企业文化</g-nav-item>
+        <g-nav-item name="developers">开发团队</g-nav-item>
+        <g-nav-item name="contacts">联系电话</g-nav-item>
+      </g-sub-nav>
       <g-nav-item name="hire">招聘</g-nav-item>
     </g-nav>
   </div>
--- src/nav/sub-nav.vue
+++ src/nav/sub-nav.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="g-sub-nav"></div>  
+  <div class="g-sub-nav">
+    <span>
+      <slot name="title"></slot>
+    </span>
+    <div class="g-sub-nav-popover">
+      <slot></slot>
+    </div>
+  </div>  
 </template>
 
 <script>
@@ -7,3 +14,17 @@ export default {
   name: "guluSubNav"
 };
 </script>
+
+<style lang="scss" scoped>
+.g-sub-nav {
+  position: relative;
+  padding: 10px 20px;
+  &-popover {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    border: 1px solid black;
+    white-space: nowrap;
+  }
+}
+</style>
